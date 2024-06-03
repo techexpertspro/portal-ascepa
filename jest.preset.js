2f43fa67dcefc0a69d63ac95eca0a1074eb29c63
Andrew Rosário <andrew.arosario@gmail.com>
2024-06-03 10:17:52 -0300

    chore: setup test coverage

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,13 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+};
