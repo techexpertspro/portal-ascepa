db1f807c92cd6139c8ca788dd5b797fd74821b20
Guilherme Toledo <gui_toledocosta@hotmail.com>
2024-06-04 16:04:16 -0300

    feat: angular material installation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes), provideAnimationsAsync(),
   ],
 };
