74386e0cf6de9e990562287eb0dec33dc45c9a54
andrewarosario <andrew.arosario@gmail.com>
2024-06-03 21:09:16 -0300

    test: fix app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,15 +9,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome portal-ascepa',
-    );
-  });
-
   it(`should have as title 'portal-ascepa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
