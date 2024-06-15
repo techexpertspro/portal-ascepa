71f9e1d52487a62f8303e4a7b2f0eb69c2a7777c
Tiago Batista <tiagobatista86@gmail.com>
2024-06-15 10:56:28 -0300

    feat: add module layout and detail for modules in readme

---
+++ modules/layout/src/lib/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {}
