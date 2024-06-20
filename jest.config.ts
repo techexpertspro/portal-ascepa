eb88c2686ac6d1f83dc97c611ace456d0cd63cd5
Tiago Batista <tiagobatista86@gmail.com>
2024-06-20 12:26:12 -0300

    Feat/feature criar layout module #55 (#63)
    Merge pr - create module layou in project

--- jest.config.ts
+++ jest.config.ts
@@ -1,26 +1,5 @@
-/* eslint-disable */
-export default {
-  displayName: 'portal-ascepa',
-  preset: './jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: './coverage/portal-ascepa',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
-};
+import { getJestProjectsAsync } from '@nx/jest';
+
+export default async () => ({
+  projects: await getJestProjectsAsync(),
+});
---
+++ modules/layout/README.md
@@ -0,0 +1,6 @@
+# layout
+
+## Module Layout
+
+Generated module layout.
+This module will have the components that will be rendered on all portal pages, such as the Header and Footer.
