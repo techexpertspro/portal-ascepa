59ad7b0e271b1a279053004ea84fc8ccb644014b
Ramon <ramonbraintrindade@gmail.com>
2024-07-23 09:12:21 -0300

    chore: remove ultimas-noticias component from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { UltimasNoticiasComponent } from '@portal-ascepa/ultimas-noticias';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, UltimasNoticiasComponent],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
