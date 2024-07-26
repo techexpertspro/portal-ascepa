0d745c8fa46c5838cee39806cb605137bd606ec1
andrewarosario <andrew.arosario@gmail.com>
2024-07-26 10:24:31 -0300

    feat: add label

---
+++ modules/shared-ui/src/lib/label/label.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-label',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './label.component.html',
+  styleUrl: './label.component.scss',
+})
+export class LabelComponent {}
