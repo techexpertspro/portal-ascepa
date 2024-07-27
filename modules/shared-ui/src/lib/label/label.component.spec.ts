d46ef07653df071d4bc7eef144b7ea184a58566e
MatheusAndradeMelo <mandrademelo77@gmail.com>
2024-07-27 11:54:41 -0300

    feat: create label component

---
+++ modules/shared-ui/src/lib/label/label.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LabelComponent } from './label.component';
+
+describe('LabelComponent', () => {
+  let component: LabelComponent;
+  let fixture: ComponentFixture<LabelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LabelComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LabelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
