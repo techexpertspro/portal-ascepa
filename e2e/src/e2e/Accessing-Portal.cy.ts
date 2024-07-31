e83b59ccc81f7ad16beecd4694e091595c2584e3
Felipe Luna <felipinho.luna@gmail.com>
2024-07-31 20:40:56 -0300

    test(cypress): implementation of initial E2E tests, accessing the ascepa portal. no Interface

---
+++ e2e/src/e2e/Accessing-Portal.cy.ts
@@ -0,0 +1,13 @@
+import { getTitle } from '../support/app.po';
+
+describe('Portal Ascepa', () => {
+  beforeEach(() => cy.visit('https://portal-ascepa.vercel.app/'));
+
+  it('Accessing Portal Ascepa', () => {
+    // Para pegar o título do H1 "Portal-Ascepa"
+    getTitle().contains('Portal-Ascepa');
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(3000);
+  });
+});
