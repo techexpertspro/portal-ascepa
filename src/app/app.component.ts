0c3733f824aa700d335b54dc5f11b530c42902ef
Luciana Rodrigues <lu.moura@gmail.com>
2024-08-15 15:49:41 -0300

    feat: adding header to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from '@portal-ascepa/layout';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
