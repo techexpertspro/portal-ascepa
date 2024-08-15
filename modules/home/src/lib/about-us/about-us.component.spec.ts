4d7a84b0d7d40b1dc99ffc58dd68886ebb9db0df
Luciana Rodrigues <lu.moura@gmail.com>
2024-08-15 15:24:27 -0300

    feat: adding about us simple tests

--- modules/home/src/lib/about-us/about-us.component.spec.ts
+++ modules/home/src/lib/about-us/about-us.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AboutUsComponent } from './about-us.component';
 
 describe('AboutUsComponent', () => {
@@ -17,5 +18,32 @@ describe('AboutUsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the h2 section title', () => {
+    const titleElement = fixture.debugElement.query(By.css('h2')).nativeElement;
+    expect(titleElement.textContent).toContain('Quem somos');
+  });
+
+  it('should render the about us source image with the correct image filename', () => {
+    component.imgSrc = 'image-placeholder.png';
+    fixture.detectChanges();
+
+    const aboutImageElement = fixture.debugElement.query(
+      By.css('img'),
+    ).nativeElement;
+    expect(aboutImageElement.getAttribute('src')).toBe(component.imgSrc);
+  });
+
+  it('should render the about us image with the correct alt text', () => {
+    component.alternativeText = 'alt teste';
+    fixture.detectChanges();
+
+    const aboutImageElement = fixture.debugElement.query(
+      By.css('img'),
+    ).nativeElement;
+    expect(aboutImageElement.getAttribute('alt')).toBe(
+      component.alternativeText,
+    );
   });
 });
