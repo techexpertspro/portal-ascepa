a804b227698e7e189ce798023534eaeff25fd4fb
Felipe Duarte Luna <felipinho.luna@gmail.com>
2024-09-16 13:42:36 -0300

    Feat/[SEO] Adicionar Meta Tags para as Pages (#171)
    Co-authored-by: charlesribeiro <charles.r.ribeiro@gmail.com>

--- modules/home/src/lib/home/home.component.spec.ts
+++ modules/home/src/lib/home/home.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Meta } from '@angular/platform-browser';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let metaService: Meta;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [Meta],
     }).compileComponents();
+
+    metaService = TestBed.inject(Meta);
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -17,5 +22,26 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add meta tags', () => {
+    // Arrange
+    component.ngOnInit();
+
+    //Act
+    const descriptionTag = metaService.getTag('name="description"');
+    const authorTag = metaService.getTag('name="author"');
+    const ogTitleTag = metaService.getTag('property="og:title"');
+
+    //Assert
+    expect(descriptionTag.content).toBe(
+      'Página inicial do Portal Ascepa. ONG Associação de e para Cegos do Pará.',
+    );
+    expect(authorTag.content).toBe(
+      'Mentoria Angular PRO | Tech Experts - https://techexperts.pro',
+    );
+    expect(ogTitleTag.content).toBe(
+      'Portal Ascepa - Associação de e para Cegos do Pará.',
+    );
   });
 });
