364d065782d9a3f2e34326e363915948f64cc7ca
charlesribeiro <charles.r.ribeiro@gmail.com>
2024-09-23 19:41:59 -0300

    :bug: fix(testing): update jest.config.app.ts

--- modules/layout/jest.config.ts
+++ modules/layout/jest.config.ts
@@ -4,6 +4,7 @@ export default {
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/modules/layout',
+  coverageReporters: ['html', 'text-summary', 'lcov'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
