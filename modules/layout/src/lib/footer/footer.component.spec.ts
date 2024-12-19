0c5c82c9409b92df4e81b4f990077a04d714d3cc
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-12-19 16:21:27 -0300

    :white_check_mark: test(testing): testes unitários de acessibilidade e elementos
    - Verificar atributos ARIA no rodapé e no link externo.
     - Garantir presença da classe `sr-only` para texto acessível escondido.
     - Validar atributos de
    segurança no link externo.
     - Testar renderização dinâmica do ano atual.
     - Assegurar presença e ordem correta dos elementos principais no rodapé.

--- modules/layout/src/lib/footer/footer.component.spec.ts
+++ modules/layout/src/lib/footer/footer.component.spec.ts
@@ -18,4 +18,43 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Verificar a presença de atributos ARIA
+  it('should have the correct aria-label', () => {
+    const footer: HTMLElement = fixture.nativeElement.querySelector('footer');
+    expect(footer.getAttribute('aria-label')).toBe(
+      'Informações sobre a ASCEPA',
+    );
+  });
+
+  it('should have the arial-label attribute in the link to Tech Experts', () => {
+    const link: HTMLElement =
+      fixture.nativeElement.querySelector('.footer-link');
+    expect(link.getAttribute('aria-label')).toBe(
+      'Tech Experts, abre em nova aba',
+    );
+  });
+
+  // Validar a presença da classe sr-only
+  it('should contain accessible text hidden with sr-only class', () => {
+    const srOnlyElement = fixture.nativeElement.querySelector('.sr-only');
+    expect(srOnlyElement).toBeTruthy();
+    expect(srOnlyElement.textContent.trim()).toBe('Tech Experts');
+  });
+
+  // Garantir que o link externo tem os atributos de segurança
+  it('should have external link attributes', () => {
+    const link: HTMLElement =
+      fixture.nativeElement.querySelector('.footer-link');
+    expect(link.getAttribute('target')).toBe('_blank');
+    expect(link.getAttribute('rel')).toBe('noopener noreferrer');
+  });
+
+  // Verficar a renderização dinâmica do ano atual
+  it('should render the current year', () => {
+    const currentYear = new Date().getFullYear();
+    const yearElement: HTMLElement =
+      fixture.nativeElement.querySelector('.footer-copyright');
+    expect(yearElement.textContent).toContain(`Copyright © ${currentYear}`);
+  });
 });
