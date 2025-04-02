0c01e4229126ea566b7a147cba3b0f5a1813cf21
Gabriel <147352311+patzsl@users.noreply.github.com>
2025-04-02 19:58:37 -0300

    :sparkles: feature: implement contact structure with responsive design
    Closes

--- modules/home/src/lib/vacancies/vacancies.component.ts
+++ modules/home/src/lib/vacancies/vacancies.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonComponent } from '@portal-ascepa/shared-ui';
 
 @Component({
   selector: 'lib-vacancies',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './vacancies.component.html',
   styleUrl: './vacancies.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
