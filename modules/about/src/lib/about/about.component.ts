94007e00bf44e04fa97561eb2876047fca3cdaef
Joao Augusto <joaoaugustofurg@gmail.com>
2025-04-02 22:11:53 -0300

    :sparkles: feature: use mock

--- modules/about/src/lib/about/about.component.ts
+++ modules/about/src/lib/about/about.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { CardComponent } from '../components/card/card.component';
+import { About } from '../interfaces/about.type';
+import { mockAbout } from '../mocks/about.mock';
 
 @Component({
   selector: 'lib-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  aboutDetail: About = mockAbout;
+}
