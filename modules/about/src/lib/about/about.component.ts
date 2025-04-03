a50f6825e1ce0025595797644531b78e02cf54bd
Guilherme André de Melo <guilhermeanm95@hotmail.com>
2025-04-03 20:02:48 -0300

    :art: style(accessibility): teste

--- modules/about/src/lib/about/about.component.ts
+++ modules/about/src/lib/about/about.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { BreakpointService } from '@portal-ascepa/shared-ui';
 import { CardComponent } from '../components/card/card.component';
 import { About } from '../interfaces/about.type';
 import { mockAbout } from '../mocks/about.mock';
@@ -13,24 +14,7 @@ import { mockAbout } from '../mocks/about.mock';
   styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-  items = [1, 2, 3, 4];
-  gridCols = 4; // Começa com 4 colunas
+  protected readonly breakpointObserver = inject(BreakpointService);
 
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    const width = window.innerWidth;
-
-    if (width > 1024) {
-      this.gridCols = 4; // 4 colunas para telas grandes
-    } else if (width > 768) {
-      this.gridCols = 2; // 2 colunas para tablets
-    } else {
-      this.gridCols = 1; // 1 coluna para celulares
-    }
-  }
-
-  constructor() {
-    this.onResize(); // Garante que a grid seja ajustada ao iniciar
-  }
   aboutDetail: About = mockAbout;
 }
