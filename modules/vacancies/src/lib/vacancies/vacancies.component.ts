ae217a8fabb1c21be52cb559896d1ce4faafa0c2
Vinícius <vini383@gmail.com>
2025-04-02 21:46:08 -0300

    :bug: fix(accessibility): fix errors from console about duplicated key
    fix errors from console about duplicated key
    
    BREAKING CHANGE: fix errors from console about duplicated key
    
    Closes #415, closes #418, closes #417, closes #419

--- modules/vacancies/src/lib/vacancies/vacancies.component.ts
+++ modules/vacancies/src/lib/vacancies/vacancies.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IVacancy } from '../../interfaces/Ivacancy.interface';
 import { VacanciesBannerComponent } from '../vacancies-banner/vacancies-banner.component';
 import { VacanciesCardComponent } from '../vacancies-card/vacancies-card.component';
@@ -12,6 +12,7 @@ import { VacanciesCardComponent } from '../vacancies-card/vacancies-card.compone
   styleUrl: './vacancies.component.scss',
 })
 export class VacanciesComponent {
+  @Input({ required: false }) hasBanner = true;
   title = 'Vagas';
 
   readonly vacancies: IVacancy[] = [
