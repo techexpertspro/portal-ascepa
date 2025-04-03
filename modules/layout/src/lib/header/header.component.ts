385342f144ef1c824a5fcd8e14a16dce4dc6e01e
charlesribeiro <charles.r.ribeiro@gmail.com>
2025-04-02 21:13:00 -0300

    :bug: fix: add changes to header

--- modules/layout/src/lib/header/header.component.ts
+++ modules/layout/src/lib/header/header.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
+
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import { SettingsComponent } from '../settings/settings.component';
 
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, SettingsComponent],
+  imports: [CommonModule, NgOptimizedImage, SettingsComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
